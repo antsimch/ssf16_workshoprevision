e9bb51851ab627f1856fa2a1e491bf55f2d377dd
antsimch <anthonysimch@gmail.com>
2023-06-06 20:29:13 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/ssf16_workshoprevision/Ssf16WorkshoprevisionApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf16_workshoprevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssf16WorkshoprevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssf16WorkshoprevisionApplication.class, args);
+	}
+
+}
